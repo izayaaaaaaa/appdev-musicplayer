dc0a36f50ccdeffa3b301e85f0ecac4f18fbe523
izayaaaaaaa <izaya.orihara0011@gmail.com>
2023-07-24 12:43:32 +0800

    main app created

---
+++ MainApp.java
@@ -0,0 +1,16 @@
+public class MainApp {
+    public static void main(String[] args) {
+        MP3PlayerModel model = new MP3PlayerModel();
+        MP3PlayerView view = new MP3PlayerView();
+        MP3PlayerController controller = new MP3PlayerController(model, view);
+
+        // Set up event listeners for user interactions
+        view.setPlayButtonListener(controller::onPlayButtonClicked);
+        view.setPauseButtonListener(controller::onPauseButtonClicked);
+        view.setSongSelectListener(controller::onSongSelected);
+
+        // Display the initial UI
+        view.showPlayButton();
+        view.showPlaylist(model.getPlaylist());
+    }
+}
