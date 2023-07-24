4018497b77387477d63a2fdd2c4e9c90d9982068
izayaaaaaaa <izaya.orihara0011@gmail.com>
2023-07-24 13:18:27 +0800

    placeholder format for the panels

--- MP3PlayerView.java
+++ MP3PlayerView.java
@@ -1,7 +1,61 @@
 // physical form
+import java.awt.*;
 import java.util.List;
 
-public class MP3PlayerView{
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MP3PlayerView extends JFrame{
+    JPanel panel1, panel2, panelA, panelB, panelC, panelD;
+
+    public MP3PlayerView() {
+        panel1 = new JPanel(new GridBagLayout());
+        panel1.setBackground(Color.BLUE);
+        GridBagConstraints c = new GridBagConstraints();
+        c.weightx = 1;
+        c.weighty = 1;
+        c.fill = GridBagConstraints.BOTH;
+        
+
+        c.gridx = 0;
+        c.gridy = 0;
+        panelA = new JPanel();
+        panelA.setBackground(Color.GREEN);
+        panel1.add(panelA, c);
+
+        c.gridx = 0;
+        c.gridy = 1;
+        panelB = new JPanel();
+        panelB.setBackground(Color.YELLOW);
+        panel1.add(panelB, c);
+
+        c.gridx = 0;
+        c.gridy = 2;
+        panelC = new JPanel();
+        panelC.setBackground(Color.ORANGE);
+        panel1.add(panelC, c);
+
+        c.gridx = 0;
+        c.gridy = 3;
+        panelD = new JPanel();
+        panelD.setBackground(Color.PINK);
+        panel1.add(panelD, c);
+
+
+        panel2 = new JPanel(new BorderLayout());
+        panel2.setBackground(Color.RED);
+
+        this.setLayout(new GridLayout(1, 2));
+        this.add(panel1);
+        this.add(panel2);
+
+        this.setTitle("MP3 Player");
+        this.setSize(500, 500);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+
     public void showCurrentSong(String songName) {
         // Display the current song on the UI
 
--- MainApp.java
+++ MainApp.java
@@ -2,12 +2,12 @@ public class MainApp {
     public static void main(String[] args) {
         MP3PlayerModel model = new MP3PlayerModel();
         MP3PlayerView view = new MP3PlayerView();
-        MP3PlayerController controller = new MP3PlayerController(model, view);
+        // MP3PlayerController controller = new MP3PlayerController(model, view);
 
         // Set up event listeners for user interactions
-        view.setPlayButtonListener(controller::onPlayButtonClicked);
-        view.setPauseButtonListener(controller::onPauseButtonClicked);
-        view.setSongSelectListener(controller::onSongSelected);
+        // view.setPlayButtonListener(controller::onPlayButtonClicked);
+        // view.setPauseButtonListener(controller::onPauseButtonClicked);
+        // view.setSongSelectListener(controller::onSongSelected);
 
         // Display the initial UI
         view.showPlayButton();
