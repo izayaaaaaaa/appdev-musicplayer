e8eb18adb9e8734655116e66ec07ea5d313529d0
vladasblood <vladimirguerrero22@gmail.com>
2023-07-24 12:44:21 +0800

    getter-setters

--- MP3PlayerModel.java
+++ MP3PlayerModel.java
@@ -1,7 +1,29 @@
+import java.util.List;
+
 public class MP3PlayerModel {
     private boolean isPlaying;
     private String currentSong;
     private List<String> playlist;
+    
+    public boolean isPlaying() {
+        return isPlaying;
+    }
+    public void setPlaying(boolean isPlaying) {
+        this.isPlaying = isPlaying;
+    }
+    public String getCurrentSong() {
+        return currentSong;
+    }
+    public void setCurrentSong(String currentSong) {
+        this.currentSong = currentSong;
+    }
+    public List<String> getPlaylist() {
+        return playlist;
+    }
+    public void setPlaylist(List<String> playlist) {
+        this.playlist = playlist;
+    }
 
     // Constructor, getters, setters, and methods for managing the MP3 player data
+    
 }
--- MP3PlayerView.java
+++ MP3PlayerView.java
@@ -1,19 +1,29 @@
-public class MP3PlayerView {
+import java.util.List;
+
+public class MP3PlayerView{
     public void showCurrentSong(String songName) {
         // Display the current song on the UI
+
     }
 
     public void showPlayButton() {
         // Display the play button on the UI
+
     }
 
     public void showPauseButton() {
         // Display the pause button on the UI
+
     }
 
     public void showPlaylist(List<String> playlist) {
         // Display the playlist on the UI
+
     }
 
     // Other methods to handle user interactions and display updates
+
+
+
+
 }
