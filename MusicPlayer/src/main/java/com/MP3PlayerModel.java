31eae9f3f30473daaa5bee28799756c7e55ced42
Franz <franzeleccion@gmail.com>
2023-08-03 15:31:28 +0800

    Fixed model and playback features(bug: audiosize too big)

--- MusicPlayer/src/main/java/com/MP3PlayerModel.java
+++ MusicPlayer/src/main/java/com/MP3PlayerModel.java
@@ -29,25 +29,26 @@ import javax.sound.sampled.Clip;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.ImageIcon;
 
-import javafx.scene.image.Image;
+// import javafx.scene.image.Image;
 
 
 public class MP3PlayerModel {
   Boolean songSelected = false,
-          songPaused = false;
+          songPaused = false,
+          songChanged = false;
+
   Long currentFrame;
   String sname;
   //String lyrics;
 
   //public String lyrics;
-  public Song s;
   public AudioInputStream songStream ;
   public Clip songClip;
   public FileReader reader;
   public BufferedReader br;
   public StringBuffer sb;
 
-  public int s_id = 1;
+  public int s_id = 0;
   
   
   /*
@@ -64,31 +65,51 @@ public class MP3PlayerModel {
   //   return s.getArtistName();
   // }
 
-  public String fetchAlbumCover(int id){
+  public void indexChanged(int id){
+    if(s_id != id){
+      songChanged = true;
+      System.out.println("Song changed");
+    }
+  }
 
+  //////////////////////////////////////////////////////////////////////
+  public void fetchID(int id){    
+    //Set to first song of no specific song has been selected
+    s_id = id;
+  }
+
+  /////////////////////////////////////////////////////////////////////
+  public Song getSong(){
     EntityManagerFactory emf = Persistence.createEntityManagerFactory("musicplayer");
     EntityManager em = emf.createEntityManager();
 
-    s = em.find(Song.class,s_id);
+    if(songChanged){
+      fetchID(s_id);
+    }
+
+    Song s = em.find(Song.class, s_id);
+
+    if (songClip == null || songStream == null || !songClip.isRunning() && s_id != -1) {
+        try {
+            stop();
+            songStream = AudioSystem.getAudioInputStream(new File(s.getSongPath()).getAbsoluteFile());
+            songClip = AudioSystem.getClip();
+            songClip.open(songStream);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    return s;
+  }
+
+  public String fetchAlbumCover(int id){
+    Song s = getSong();
 
     return s.getAlbumCover();
   }
 
   public String lyricsProcess(int id){
-    
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("musicplayer");
-    EntityManager em = emf.createEntityManager();
-
-    int j;
-
-    if(id <= -1){
-      s_id = 1;
-    }
-    else{
-      s_id = id;
-    }
-
-    s = em.find(Song.class, s_id);
+    Song s = getSong();
 
     try {
       
@@ -114,368 +135,100 @@ public class MP3PlayerModel {
   }
 
 
+  public void playMusic() {
+    System.out.println("Music played: s_id " + s_id);
 
-  public void fetchSong(int id){
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("musicplayer");
-    EntityManager em = emf.createEntityManager();
-    
+    Song s = getSong();
 
-    if(id <= -1){
-      s_id = 1;
+    if (!songSelected && !songPaused && s != null) {
+        songClip.start();
+        songPaused = false;
+        songSelected = true;
+    } else if (songPaused && s != null) {
+        resumePlay();
+        songPaused = false;
     }
-    else{
-      s_id = id;
-    }
-
-    s = em.find(Song.class, s_id);
-
-    try{
-
-      songStream = AudioSystem.getAudioInputStream(new File(s.getSongPath()).getAbsoluteFile());
-      songClip = AudioSystem.getClip();
-      songClip.open(songStream);
-
-    } catch (Exception ex) {
-
-      ex.printStackTrace();
-    }
-    
   }
+
 
   public void pauseMusic(){
+    Song s = getSong();
 
-      EntityManagerFactory emf = Persistence.createEntityManagerFactory("musicplayer");
-      EntityManager em = emf.createEntityManager();
-
-    try {
-      em.getTransaction().begin();
-
-      System.out.println("Song ID:" + s.getId());
-
-      if (s != null && songClip.isRunning()) {
-
+    if (s != null && songClip.isRunning()) {
         currentFrame = songClip.getMicrosecondPosition();
         System.out.println("Current Frame is " + currentFrame);
-
         songClip.stop();
         songPaused = true;
-      }
-
-      em.getTransaction().commit();
-
-    } catch (Exception e) {
-      // TODO: handle exception
-
-      if(em.getTransaction() != null){
-        em.getTransaction().rollback();
-      }
-
-      e.printStackTrace();
-    } finally {
-      em.close();
-      emf.close();
     }
   }
-
-  /*
-   * PLAY MUSIC
-   */
-
-  public void playMusic() {
-    System.out.println("playMusic model method");
-
-      EntityManagerFactory emf = Persistence.createEntityManagerFactory("musicplayer");
-      EntityManager em = emf.createEntityManager();
-
-      try {
-
-        em.getTransaction().begin();
-        fetchSong(s_id);
-        //lyricsProcess(s_id);
-        
-
-        if (!songSelected) {  
-
-          System.out.println("Song Path: " + s.getSongPath());
-          fetchSong(s_id);   
-          // reader = new FileReader(new File(s.getLyricsPath()).getAbsolutePath());
-          // br = new BufferedReader(reader);
-          // System.out.println("Lyrics: "  + br.readLine());
-          // br.close();
-
-
-          songClip.start();
-          songSelected = true;  // Song has been selected
-          songPaused = false;   // Song is not paused
-
-        } else if (songSelected && songPaused && s != null){
-          resumePlay();
-        }
-
-        //lyricsProcess(s_id);
-
-        // while(lyricsProcess(s_id) == null){
-        //   break;
-        // }
-
-      } catch (Exception ex) {
-
-        if(em.getTransaction() != null){
-
-          em.getTransaction().rollback();
-
-        }
-          ex.printStackTrace();
-
-      } finally {
-        em.close();
-        emf.close();
-      }
-
-  }
-
-  /*
-   * RESUME MUSIC
-   */
 
   public void resumePlay() {
-
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("musicplayer");
-    EntityManager em = emf.createEntityManager();
-
-    try {
-
-      em.getTransaction().begin();
-
-
-      if(s != null){
-
+    Song s = getSong();
+    if (s != null) {
         try {
-          songClip.close();
-          resetAudioStream();
-          songClip.setMicrosecondPosition(currentFrame);
-          songClip.start();
-          songPaused = false;
-          
-        } catch (Exception e1) {
-
-          System.out.println("resumePlay() error");
-
-          e1.printStackTrace();
-
+            songStream = AudioSystem.getAudioInputStream(new File(s.getSongPath()).getAbsoluteFile());
+            songClip = AudioSystem.getClip();
+            songClip.open(songStream);
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
-      }
-
-      
-    } catch (Exception e) {
-
-        if(em.getTransaction() != null){
-
-          em.getTransaction().rollback();
-
+        if(songChanged){
+          currentFrame = 0L;
+          songChanged = false;
         }
-
-        e.printStackTrace();
-
-    } finally {
-      em.close();
-      emf.close();
+        songClip.setMicrosecondPosition(currentFrame); // Set the clip's position to the previously paused position
+        songClip.start();
+        
     }
-
-  }
-
-  /*
-   * RESET AUDIO STREAM MUSIC
-   */
-  
-  public void resetAudioStream(){
-
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("musicplayer");
-    EntityManager em = emf.createEntityManager();
-
-    try {
-      
-      em.getTransaction().begin();
-
-      s = em.find(Song.class, s_id);
-
-      if (s != null){
-  
-        fetchSong(s_id);
-
-      }
-
-
-    } catch (Exception e) {
-      // TODO: handle exception
-      if(em.getTransaction() != null){
-          em.getTransaction().rollback();
-      }
-
-      e.printStackTrace();
-
-    } finally {
-      em.close();
-      emf.close();
-    }
-
   }
 
   public void fastForwardMusic() {
+    Song s = getSong();
+    if (s != null){
 
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("musicplayer");
-    EntityManager em = emf.createEntityManager();
-
-    try {
-
-        em.getTransaction().begin();
-
-      if (s != null){
-
-        long currentPosition = songClip.getMicrosecondPosition();
-        long newPosition = currentPosition + 1000000;
-        
-        if(newPosition > songClip.getMicrosecondLength()){
-          newPosition = songClip.getMicrosecondLength();
-        }
-
-        songClip.setMicrosecondPosition(newPosition);
-
+      long currentPosition = songClip.getMicrosecondPosition();
+      long newPosition = currentPosition + 1000000;
+      
+      if(newPosition > songClip.getMicrosecondLength()){
+        newPosition = songClip.getMicrosecondLength();
       }
 
+      songClip.setMicrosecondPosition(newPosition);
 
-    } catch (Exception e) {
-      // TODO: handle exception
-
-      if(em.getTransaction() != null){
-          em.getTransaction().rollback();
-      }
-
-      e.printStackTrace();
-
-    } finally {
-      em.close();
-      emf.close();
     }
   }
 
   public void backtrackMusic() {
+    Song s = getSong();
+    if (s != null){
 
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("musicplayer");
-    EntityManager em = emf.createEntityManager();
-
-    try {
-
-      em.getTransaction().begin();
-
-      if (s != null){
-
-        long currentPosition = songClip.getMicrosecondPosition();
-        long newPosition = currentPosition - 1000000;
-        
-        if(newPosition < 0){
-          newPosition = 0;
-        }
-        songClip.setMicrosecondPosition(newPosition);
-
-      }
+      long currentPosition = songClip.getMicrosecondPosition();
+      long newPosition = currentPosition - 1000000;
       
-    } catch (Exception e) {
-      // TODO: handle exception
-
-      if(em.getTransaction() != null){
-          em.getTransaction().rollback();
+      if(newPosition < 0){
+        newPosition = 0;
       }
+      songClip.setMicrosecondPosition(newPosition);
 
-      e.printStackTrace();
-    } finally {
-      em.close();
-      emf.close();
-    }
-
-    if (s != null) {
-      // long currentPosition = s.getSongClip().getMicrosecondPosition();
-      // long newPosition = currentPosition - 1000000; // Adjust the value as needed for the backtrack speed
-      // if (newPosition < 0) {
-      //   newPosition = 0; // Make sure we don't go before the start of the song
-      // }
-      // s.getSongClip().setMicrosecondPosition(newPosition);
     }
   }
 
-  public void nextSongMusic(){
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("musicplayer");
-    EntityManager em = emf.createEntityManager();
-    Query query = em.createQuery("SELECT COUNT(s) FROM Song s");
-    Long recordCount = (Long) query.getSingleResult();
-
-    try {
-
-      songClip.stop();
-      em.getTransaction().begin();
-      s_id++;
-
-      //Reaches MAX Highest ID
-      if(s_id >= 4 || s_id == 3){
-        s_id=0;
-        System.out.println("Test 1: "+ s_id);
-      } 
-
-      fetchSong(s_id);
-      songClip.start();
-
-
-    } catch (Exception e) {
-      // TODO: handle exception
-      if(em.getTransaction() != null){
-        em.getTransaction().rollback();
-      }
-
-      e.printStackTrace();
-    } finally {
-      em.close();
-      emf.close();
-    }
-  }
-
-  public void previousSongMusic(){
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("musicplayer");
-    EntityManager em = emf.createEntityManager();
-    Query query = em.createQuery("SELECT COUNT(s) FROM Song s");
-    Long recordCount = (Long) query.getSingleResult();
-
-    try {
-
-      songClip.stop();
-      em.getTransaction().begin();
-      s_id--;
-
-      //Reaches MAX Lowest ID
-      if(s_id >= 4 || s_id == -1){
-        s_id=2;
-        System.out.println("Test 1: "+ s_id);
-      } 
-
-      fetchSong(s_id);
-      songClip.start();
-
-      
-    } catch (Exception e) {
-      // TODO: handle exception
-      if(em.getTransaction() != null){
-        em.getTransaction().rollback();
-      }
-
-      e.printStackTrace();
-    } finally {
-      em.close();
-      emf.close();
-    }
-  }
 
   public void stop() {
-    if (s != null) {
-      //s.getSongClip().stop();
+    if (songClip != null && songClip.isRunning()) {
+      songClip.setMicrosecondPosition(0);
+      songClip.stop();
+      songClip.close();
+      try {
+        songStream.close();
+      } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+      songStream = null;
+      songClip = null;
+      songSelected = false;
+      songPaused = false;
     }
   }
 
@@ -495,20 +248,13 @@ public class MP3PlayerModel {
   }
 
   public String fetchSongName(int id) {
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("musicplayer");
-    EntityManager em = emf.createEntityManager();
-
-    s = em.find(Song.class, s_id);
+    Song s = getSong();
 
     return s.getSongTitle();
   }
 
   public String fetchArtistName(int id) {
-
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("musicplayer");
-    EntityManager em = emf.createEntityManager();
-
-    s = em.find(Song.class, s_id);
+    Song s = getSong();
 
     return s.getArtistName();
   }
